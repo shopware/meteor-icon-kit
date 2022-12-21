9f71704768b25ba8c30cd100a4c146d4399b8b84
Bojan Rajh <b.rajh@shopware.com>
2022-12-21 12:11:11 +0100

    Disable build step

--- .github/scripts/docs-after-clone.sh
+++ .github/scripts/docs-after-clone.sh
@@ -13,6 +13,9 @@ echo "Building figma"
 pnpm run --dir /tmp/mount-all start
 #npm run --prefix /tmp/mount-all start
 
-echo "Running docs build"
-pnpm run --dir /tmp/mount-all/docs build
+echo "Copying icons"
+cp -R /tmp/mount-all/icons /tmp/mount-all/docs/public
+
+#echo "Running docs build"
+# pnpm run --dir /tmp/mount-all/docs build
 #npm run --prefix /tmp/mount-all/docs build
