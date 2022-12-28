e9d9179df93242417c6caad4e22d5c96aca26af3
Bojan Rajh <b.rajh@shopware.com>
2022-12-28 12:50:53 +0100

    Cleanup

--- .github/scripts/docs-after-clone.sh
+++ .github/scripts/docs-after-clone.sh
@@ -3,19 +3,12 @@ set -e
 
 echo "Installing pnpm"
 pnpm i --dir /tmp/mount-all
-#npm i --prefix /tmp/mount-all
 
 echo "Running root build"
 pnpm run --dir /tmp/mount-all build
-#npm run --prefix /tmp/mount-all build
 
 echo "Building figma"
 pnpm run --dir /tmp/mount-all start
-#npm run --prefix /tmp/mount-all start
 
 echo "Copying icons"
 cp -R /tmp/mount-all/icons /tmp/mount-all/docs/public
-
-#echo "Running docs build"
-# pnpm run --dir /tmp/mount-all/docs build
-#npm run --prefix /tmp/mount-all/docs build
